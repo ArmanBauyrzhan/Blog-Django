ac006527412a861be903c0eb16f8bd69a3074842
anon123 <anon123@gmail.com>
2024-11-18 11:20:15 +0500

    Добавил базовые маршруты, создал шаблон base.html и home.html, Сделал базовые функции представления для домашней страницы и страницы 404

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+menu = [{'title': 'О сайте', 'urlname': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'PostForm'},
+        {'title': 'Выйти', 'url_name': 'logout'},
+        {'title': 'Войти', 'urlname': 'login'},
+]
+
+def home(request):
+    context = {
+        'menu': menu,
+    }
+    return render(request, 'blog/home.html', context=context)
+
+def pageNotFound(request, exception):
+    return HttpResponse('Page not found')
